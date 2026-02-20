4db5d033eb11d8f352030b946bf600dfe73beb79
Titus Kruse <mail@tituskruse.de>
2026-02-20 17:27:55 +0100

    Bump to 0.24.6

--- plugins/module_utils/constants.py
+++ plugins/module_utils/constants.py
@@ -1,4 +1,4 @@
-COLLECTION_VERSION = "0.24.5"
+COLLECTION_VERSION = "0.24.6"
 COLLECTION_MIN_STEP_CLI_VERSION = "0.24.0"
 COLLECTION_REPO = "https://github.com/maxhoesel-ansible/ansible-collection-smallstep"
 
